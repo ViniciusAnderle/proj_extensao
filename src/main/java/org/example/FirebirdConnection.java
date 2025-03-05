603117712141d0fd6c9e5d6a9f9f00b82c7c27ad
ViniciusAnderle <vinianderle00@gmail.com>
2025-03-05 00:19:01 -0300

    SELECT PRODUCTS

--- src/main/java/org/example/FirebirdConnection.java
+++ src/main/java/org/example/FirebirdConnection.java
@@ -2,25 +2,34 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class FirebirdConnection {
     public static void main(String[] args) {
-        // URL de conexão com o banco
         String url = "jdbc:firebirdsql://localhost:3050/" + System.getProperty("user.home") + "/sol.fdb";
-        // Credenciais de acesso
         String user = "SYSDBA";
         String password = "masterkey";
 
-        try {
-            // Estabelece a conexão
-            Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement()) {
 
-            // Se a conexão for bem-sucedida
             System.out.println("Conectado ao banco Firebird com sucesso!");
 
-            // Feche a conexão quando não for mais necessária
-            connection.close();
+            String sql = "SELECT ID_PRODUTO, DESCRICAO, PERC_IPI FROM TPRODUTOS;";
+
+            try (ResultSet resultSet = statement.executeQuery(sql)) {
+                System.out.println("Dados da tabela TPRODUTOS:");
+                while (resultSet.next()) {
+                    int idProduto = resultSet.getInt("ID_PRODUTO");
+                    String descricao = resultSet.getString("DESCRICAO");
+                    double percIpi = resultSet.getDouble("PERC_IPI");
+
+                    System.out.println("ID_PRODUTO: " + idProduto + ", DESCRICAO: " + descricao + ", PERC_IPI: " + percIpi);
+                }
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
